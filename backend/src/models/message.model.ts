c11e1388eda2c8c96b6b9944e7d87ebd1e889b3e
Bagus Nanda <baguszz@gmail.com>
2019-05-27 12:35:34 +0200

    create models for message and user

--- backend/src/models/message.model.ts
+++ backend/src/models/message.model.ts
@@ -1,3 +1,91 @@
 /**
  * Model for Message
  */
+
+import * as mongoose from 'mongoose';
+
+/**
+ * @description interface for message.
+ * @export
+ * @interface IMessageModel
+ * @extends {mongoose.Document}
+ */
+export interface IMessageModel extends mongoose.Document {
+    title: string;
+    description: string;
+    creatorid: number;
+    lon: number;
+    lat: number;
+    address: string;
+    available: boolean;
+    archive: boolean;
+    image: string;
+    phone?: number;
+    created_at: Date;
+    updated_at: Date;
+}
+
+/**
+ * @description Schema of message for mongoose.
+ * @export
+ * @Schema messageSchema 
+ */
+export const messageSchema: mongoose.Schema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        creatorid: {
+            type: Number,
+            required: true,
+        },
+        lon: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        lat: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        available: {
+            type: Boolean,
+            required: true,
+        },
+        archive: {
+            type: Boolean,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: Number,
+            required: false,
+        },
+    },
+    {
+        timestamps: {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at',
+        },
+    }
+);
+
+/**
+ * @description Export the message mongoose Schema.
+ * @export
+ * @Model message 
+ */
+export const message: mongoose.Model<IMessageModel> = mongoose.model<IMessageModel>('Message', messageSchema);
--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,3 +1,52 @@
 /**
  * Model for User
  */
+
+import * as mongoose from 'mongoose';
+
+/**
+ * @description interface for user.
+ * @export
+ * @interface IUserModel
+ * @extends {mongoose.Document}
+ */
+export interface IUserModel extends mongoose.Document {
+    username: string;
+    token: string;
+    password: string;
+    created_at: Date;
+}
+
+/**
+ * @description Schema of user for mongoose.
+ * @export
+ * @Schema userSchema 
+ */
+export const userSchema: mongoose.Schema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        token: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: {
+            createdAt: 'created_at',
+        },
+    }
+);
+
+/**
+ * @description Export the user mongoose Schema.
+ * @export
+ * @Model user 
+ */
+export const user: mongoose.Model<IUserModel> = mongoose.model<IUserModel>('User', userSchema);
