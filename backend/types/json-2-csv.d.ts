8fc69eed14158f7e5207870d814818acb336364f
Manh Ton Nguyen <manhton.t.nguyen@stud.h-da.de>
2019-05-28 16:37:50 +0200

    fix some error lib Backend

---
+++ backend/types/json-2-csv.d.ts
@@ -0,0 +1,6 @@
+export function csv2json(data: any, callback: any, options: any): void;
+export function csv2jsonAsync(csv: any, options: any): void;
+export function csv2jsonPromisified(csv: any, options: any): void;
+export function json2csv(data: any, callback: any, options: any): void;
+export function json2csvAsync(json: any, options: any): void;
+export function json2csvPromisified(json: any, options: any): void;
