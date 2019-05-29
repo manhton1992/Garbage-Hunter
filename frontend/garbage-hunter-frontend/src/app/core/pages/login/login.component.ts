3bcc37ad95447297d65c59081fc7e87a7709c971
Tai Linh Du <tai-linh.du@arithnea.de>
2019-05-29 12:09:01 +0200

    implement login and register component

--- frontend/garbage-hunter-frontend/src/app/core/pages/login/login.component.ts
+++ frontend/garbage-hunter-frontend/src/app/core/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  private userLogin = {
+    email : "",
+    password : "",
+  };
+  constructor(private userService : UserService) { }
 
+   
   ngOnInit() {
+  }
+
+  submitLogin(){
+    let response: any ;
+    this.userService.getUser(this.userLogin.email,this.userLogin.password).subscribe(res => {
+      response = res;
+      console.log(res);
+      if (response != null){
+        if(response.status != null && response.status == 'success'){
+          this.userService.user = response.docs; 
+          console.log("login success");
+          
+        } else if (response.status != null && response.status == 'fail'){
+          console.log("false input or user does not exist");
+        } else {
+          console.log("error when einlogging")
+        }
+  
+      }
+    });
+ 
   }
 
 }
