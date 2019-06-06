0e914562c4c8d2d3bab9eed915d28ef93f556d75
Manh Ton Nguyen <manhton.t.nguyen@stud.h-da.de>
2019-06-06 17:33:17 +0200

    Complete Message Detail Update, Delete, go Back , responsive

--- frontend/garbage-hunter-frontend/src/app/core/pages/show-message/show-message.component.ts
+++ frontend/garbage-hunter-frontend/src/app/core/pages/show-message/show-message.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {MessageService} from "../../../services/message/message.service";
+import {Message} from "../../../models/message.model";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-show-message',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-message.component.scss']
 })
 export class ShowMessageComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() message : Message;
+  constructor(
+    private messageService : MessageService,
+    private location : Location
+  ) { }
   ngOnInit() {
+    this.getMessage();
   }
-
+  getMessage() : void {
+    let stringToSplit = window.location.href;
+    let x = stringToSplit.split("/");
+    this.messageService.getMessageById(x[x.length-1]).subscribe(message => this.message = message);
+  }
+  goBack() : void {
+    this.location.back();
+  }
+  upDate() : void {
+    this.messageService.updateMessage(this.message).subscribe(() => this.goBack());
+  }
+  deleteMessage() : void {
+    this.messageService.deleteMessage(this.message._id.toString()).pipe().subscribe(() =>this.goBack());
+  }
 }
