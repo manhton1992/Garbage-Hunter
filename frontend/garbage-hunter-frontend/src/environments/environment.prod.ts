37c686932dee093ef090df62486fefaa84983c49
Bagus Nanda <baguszz@gmail.com>
2019-06-14 18:54:48 +0200

    dockerized app

--- frontend/garbage-hunter-frontend/src/environments/environment.prod.ts
+++ frontend/garbage-hunter-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://ec2-18-184-142-163.eu-central-1.compute.amazonaws.com:3000/api',
+  baseUrl: 'http://localhost:3000/api',
+  // baseUrl: 'http://ec2-18-184-142-163.eu-central-1.compute.amazonaws.com:3000/api',
   nominatimUrl: 'https://nominatim.openstreetmap.org',
 };
