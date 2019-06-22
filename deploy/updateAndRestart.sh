1bbe78c5ada8f68492667d0acd95c4c87279b677
Tai Linh Du <tai-linh.du@arithnea.de>
2019-06-22 18:10:47 +0200

    #68 add auto deploy code in aws

---
+++ deploy/updateAndRestart.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# any future command that fails will exit the script
+set -e
+
+# Delete the old repo
+# rm -rf /garbage-hunter
+
+# go the project folder
+cd garbage-hunter
+
+# down the docker compose
+sudo docker-compose down
+
+# checkout to master branch
+git checkout master
+
+# pull the new code
+git pull origin master
+
+# build the docker compose
+sudo docker-compose build
+
+# run the docker compose
+sudo docker-compose up
