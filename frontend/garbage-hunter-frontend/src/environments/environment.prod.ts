d97f130165e947651f9129db0d516558d03de2c6
Tai Linh Du <tai-linh.du@arithnea.de>
2019-06-25 00:36:35 +0200

    add new aws ip

--- frontend/garbage-hunter-frontend/src/environments/environment.prod.ts
+++ frontend/garbage-hunter-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // aws instance
-  baseUrl: 'http://18.185.126.85:3000/api',
+  baseUrl: 'http://18.184.237.145:3000/api',
   // baseUrl: 'http://ec2-18-184-142-163.eu-central-1.compute.amazonaws.com:3000/api',
   nominatimUrl: 'https://nominatim.openstreetmap.org',
 };
